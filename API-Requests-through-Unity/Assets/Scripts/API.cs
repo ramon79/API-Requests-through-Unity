a51dc7ebe793ddab88dd740578cc35154468f2f5
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-03-04 22:26:42 +0900

    Toggle Button using UI button

--- API-Requests-through-Unity/Assets/Scripts/API.cs
+++ API-Requests-through-Unity/Assets/Scripts/API.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 
 public class API : MonoBehaviour 
 {
-	private const string URL = "http://n3k.ca";
+	private const string URL = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate=2017-04-19&ServiceKey=KD9z5m4PSjedJaRquWlGpAlJ5LFEFHhvN1c2mSI9TgkZKBjewHTtvvKz66c6ms99P09gzaIXEatfs8o4s7fjcA%3D%3D";
+	private const string API_KEY = "KD9z5m4PSjedJaRquWlGpAlJ5LFEFHhvN1c2mSI9TgkZKBjewHTtvvKz66c6ms99P09gzaIXEatfs8o4s7fjcA%3D%3D";
 	public Text responseText;
 
 	public void Request()
