4a199d56d359238c4102a5b7a615067d14828678
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-03-04 09:16:09 +0900

    first part

---
+++ API-Requests-through-Unity/Assets/Scripts/API.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class API : MonoBehaviour 
+{
+	private const string URL = "http://n3k.ca";
+	public Text responseText;
+
+	public void Request()
+	{
+		WWW request = new WWW(URL);
+		StartCoroutine(OnResponse(request));
+
+
+	}
+
+	private IEnumerator OnResponse(WWW req)
+	{
+		yield return req;
+		responseText.text = req.text;
+	}
+
+
+
+
+}
